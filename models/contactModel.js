f415247a66ee90736f494f85f407b716cde06690
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2024-04-04 22:01:00 +0200

    add model and schema

---
+++ models/contactModel.js
@@ -0,0 +1,20 @@
+import { model, Schema } from "mongoose";
+
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+export const Contact = model("contacts", contactSchema);
