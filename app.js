7e8b8f537605e4e43e77dc57415bd147074165ba
Yelyzaveta1224 <demcenkoelizaveta6@gmail.com>
2024-04-18 23:26:12 +0200

    345

--- app.js
+++ app.js
@@ -29,6 +29,8 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(express.static("public"));
+
 app.use("/api/contacts", contactsRouter);
 
 app.use("/users", usersRouter);
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 import { model, Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import crypto from "crypto";
 
 const userSchema = new Schema({
   password: {
@@ -20,9 +21,16 @@ const userSchema = new Schema({
     type: String,
     default: null,
   },
+  avatarURL: String,
 });
 
 userSchema.pre("save", async function (next) {
+  if (this.isNew) {
+    const emailHash = crypto.createHash("md5").update(this.email).digest("hex");
+
+    this.avatarURL = `https://gravatar.com/avatar/${emailHash}.jpg?d=robohash`;
+  }
+
   if (!this.isModified("password")) return next();
 
   const salt = await bcrypt.genSalt(10);
